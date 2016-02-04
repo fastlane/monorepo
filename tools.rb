5dc9288a79fd59f582114d4962e121305e99f7a7
KrauseFx <KrauseFx@gmail.com>
2016-02-03 16:27:09 -0800

    Moved list of tools into its own file

---
+++ tools.rb
@@ -0,0 +1,2 @@
+@tools = %w(fastlane fastlane_core deliver snapshot frameit pem sigh produce cert gym pilot credentials_manager scan supply watchbuild match spaceship)
+@tools = ['watchbuild', 'match', 'deliver']
