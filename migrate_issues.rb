4f38c586a243e8d6ffcd3067d114e18e306cff31
Felix Krause <krausefx@gmail.com>
2016-02-09 13:00:05 -0800

    Migrated migrate_issues to use the new GH import API

--- migrate_issues.rb
+++ migrate_issues.rb
@@ -1,18 +1,18 @@
 require 'octokit'
 require 'pry'
+require 'excon'
+require 'colored'
+require 'json'
+
 
 class Hendl
   # e.g. KrauseFx/fastlane
   attr_accessor :source
   attr_accessor :destination
 
-  # Reason on why this was necessary
-  attr_accessor :reason
-
-  def initialize(source: nil, destination: nil, reason: nil)
+  def initialize(source: nil, destination: nil)
     self.source = source
     self.destination = destination
-    self.reason = reason
     self.start
   end
 
@@ -23,6 +23,7 @@ class Hendl
   def start
     client.issues(source, per_page: 1000).each do |original|
       hendl(original)
+      smart_sleep
     end
   end
 
@@ -45,76 +46,61 @@ class Hendl
   # We copy over all the issues, and also mention everyone
   # so that people are automatically subscribed to notifications
   def hendl_issue(original)
-    body = []
-    body << "This issue was copied from [#{source}##{original.number}](#{original.html_url}) "
-    body << "From @#{original.user.login} on #{original.created_at.strftime('%F')}"
-    body << "----"
-    body << original.body
+    original_comments = client.issue_comments(source, original.number)
+    comments = []
+    original_comments.each do |original_comment|
+      comments << {
+        created_at: original_comment.created_at.iso8601,
+        body: original_comment.body,
+        # user: original_comment.user.login
+      }
+    end
 
     tool_name_label = source.split("/").last
-    options = { labels: original.labels + [tool_name_label] }
-    options[:assignee] = original.assignee.login if original.assignee
+    data = {
+      issue: {
+        title: original.title,
+        body: original.body,
+        created_at: original.created_at.iso8601,
+        user: original.user.login,
+        labels: original.labels + [tool_name_label],
+        closed: original.state != "open"
+      },
+      comments: comments
+    }
+    data[:issue][:closed_at] = original.closed_at.iso8601 if original.state != "open"
 
-    puts "Copying issue #{original.number} from #{source}..."
-    issue = client.create_issue(destination, 
-                                original.title, 
-                                body.join("\n\n"), 
-                                options)
-    smart_sleep
+    puts data.to_s.green
+    puts ""
+    response = Excon.post("https://api.github.com/repos/#{destination}/import/issues", body: data.to_json, headers: request_headers)
+    response = JSON.parse(response.body)
+    puts response.to_s.yellow
 
-    authors = [original.user.login]
+    # TODO: link from old issue here and close the old one
+  end
 
-    original_comments = client.issue_comments(source, original.number)
-    original_comments.each do |original_comment|
-      body = []
-      body << "From @#{original_comment.user.login} on #{original_comment.created_at.strftime('%F')}"
-      body << "----"
-      body << original_comment.body
-      puts "Copying issue comment #{original_comment.id}..."
-      client.add_comment(destination, issue.number, body.join("\n\n"))
-      smart_sleep
-
-      authors << original_comment.user.login
-    end
-
-    if issue.state == 'open'
-      # TODO: improve design + wording here
-      body = ["Hello @#{authors.join(", @")}"]
-      body << "This issue was automatically migrated from [#{source}##{original.number}](#{original.html_url})."
-      body << "Please confirm that this issue is still relevant, otherwise it might automatically be closed after a while :warning:"
-      body << "Thanks for your helping making fastlane better :rocket:"
-      client.add_comment(destination, issue.number, body.join("\n\n"))
-
-      # Now it's the time to close the old issue
-      body = ["Hello @#{original.user.login},"]
-      body << reason
-      body << "This issue was automatically migrated to [#{destination}##{issue.number}](#{issue.html_url})."
-      body << "Please open the newly created issue and confirm that this ticket is still relevant, otherwise it will be closed after a while :warning:"
-      body << "Thanks for your helping making fastlane better :rocket:"
-
-      client.add_comment(source, original.number, body.join("\n\n"))
-      client.close_issue(source, original.number)
-    else
-      client.close_issue(destination, issue.number) 
-    end
-    smart_sleep
+  def request_headers
+    {
+      "Accept" => "application/vnd.github.golden-comet-preview+json",
+      "Authorization" => ("token " + ENV["GITHUB_API_TOKEN"]),
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "User-Agent" => "fastlane bot"
+    }
   end
 
   # We want to comment on PRs and tell the user to re-submit it
   # on the new repo, as we can't migrate them automatically
   def hendl_pr(original)
-    puts "#{original.number} is a pull request"
+    # puts "#{original.number} is a pull request"
 
-    body = ["Hello @#{original.user.login},"]
-    body << reason
-    body << "Sorry for the troubles, we'd appreciate if you could re-submit your Pull Request with these changes to the new repository"
+    # body = ["Hello @#{original.user.login},"]
+    # body << "Sorry for the troubles, we'd appreciate if you could re-submit your Pull Request with these changes to the new repository"
 
-    client.add_comment(source, original.number, body.join("\n\n"))
-    client.close_pull_request(source, original.number)
-    smart_sleep
+    # client.add_comment(source, original.number, body.join("\n\n"))
+    # client.close_pull_request(source, original.number)
+    # smart_sleep
   end
 end
 
 Hendl.new(source: "fastlane/playground", 
-     destination: "fastlane/playground2", 
-          reason: "`fastlane` is now a mono repo, you can read more about this decision in our [blog post](https://fastlane.tools). All tools are now available in the [fastlane main repo](https://github.com/fastlane/fastlane).")
+     destination: "fastlane/playground2")
