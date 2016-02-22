06d9b8dbcba5873a07b64ebf938fbf979059ccef
Felix Krause <krausefx@gmail.com>
2016-02-22 11:17:44 -0500

    Added screengrab

--- tools.rb
+++ tools.rb
@@ -1,2 +1,1 @@
-@tools = %w(fastlane fastlane_core deliver snapshot frameit pem sigh produce cert gym pilot credentials_manager scan supply watchbuild match spaceship)
-@tools = ['watchbuild', 'match', 'deliver']
+@tools = %w(fastlane fastlane_core deliver snapshot frameit pem sigh produce cert gym pilot credentials_manager scan supply watchbuild match spaceship screengrab)
